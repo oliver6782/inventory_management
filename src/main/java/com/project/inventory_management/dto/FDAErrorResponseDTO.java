8dd9ca33e4f3f3afb9ab08bd8f434b71f47ca2cc
Oliver6782 <xuoli@foxmail.com>
2024-07-19 21:22:04 +0800

    Modified FDAResponseErrorHandler

--- src/main/java/com/project/inventory_management/dto/FDAErrorResponseDTO.java
+++ src/main/java/com/project/inventory_management/dto/FDAErrorResponseDTO.java
@@ -7,8 +7,15 @@ import lombok.Data;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class FDAErrorResponseDTO {
+    private FDAError error;
     // deleted Error class
     private String code;
     private String message;
 
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class FDAError {
+        private String code;
+        private String message;
+    }
 }
